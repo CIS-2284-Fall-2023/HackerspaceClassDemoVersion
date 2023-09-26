c2cbb0129910943888dcec05b1d484d1efc954a9
Rob <rgarner011235@gmail.com>
2023-09-25 19:00:28 -0600

    Added Form Data Class

---
+++ Hackerspace/Shared/Models/FormData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerspace.Shared.Models
+{
+    public class FormData
+    {
+        public string Text { get; set; }
+        public bool IsSet { get; set; }
+        public string TextArea { get; set; }
+        public string RadioGroupSelection { get; set; }
+        public string DropDownSelection { get; set; }
+        public DateTime DateTimeSelected { get; set; }
+    }
+}
