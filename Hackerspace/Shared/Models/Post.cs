cc5843794ddb10101fee8e4fb22c37bea7f26ef1
Rob <rgarner011235@gmail.com>
2023-10-25 17:53:04 -0600

    Modified a few things

--- Hackerspace/Shared/Models/Post.cs
+++ Hackerspace/Shared/Models/Post.cs
@@ -1,9 +1,16 @@
-﻿namespace Hackerspace.Shared.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hackerspace.Shared.Models
 {
     public class Post
     {
+        
         public int Id { get; set; }
+        [Required]
+        [MinLength(5)]
         public string Title { get; set; } = "";
+        [Required]
+        [MinLength(20)]
         public string Text { get; set; } = "";
         public DateTime Date { get; set; } = DateTime.Now;
     }
